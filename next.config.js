4ab5814f59cd6df956246fbac9972afdbc131cb3
gavinarori <arorigavin@gmail.com>
2024-08-25 21:19:18 +0300

    first commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["lh3.googleusercontent.com",'img.freepik.com',"d1eipm3vz40hy0.cloudfront.net","pbs.twimg.com","images.unsplash.com","encrypted-tbn0.gstatic.com"],
+  },
+}
+
+module.exports = nextConfig
