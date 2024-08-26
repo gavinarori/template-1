474bc1c3c88e7f0d586d54e8e45b3226f7bc4384
gavinarori <arorigavin@gmail.com>
2024-08-26 12:30:32 +0300

    added content to the hero section

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useState, useEffect } from "react";
 import { PEOPLE_URL } from "@/constants";
 import Image from "next/image";
@@ -12,42 +12,42 @@ interface CampProps {
 
 const CampSite = ({ backgroundImage, title, subtitle, peopleJoined }: CampProps) => {
   return (
-    <div className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}>
-     <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10">
-      <div className="flexCenter gap-4">
-        <div className="rounded-full bg-green-50 p-4">
-          <Image
-            src="/folded-map.svg"
-            alt="map"
-            width={28}
-            height={28}
-          />
-        </div>
-        <div className="flex flex-col gap-1">
-          <h4 className="bold-18 text-white">{title}</h4>
-          <p className="regular-14 text-white">{subtitle}</p>
-        </div>
-      </div>
-
-      <div className="flexCenter gap-6">
-        <span className="flex -space-x-4 overflow-hidden">
-          {PEOPLE_URL.map((url) => (
-            <Image 
-              className="inline-block h-10 w-10 rounded-full"
-              src={url}
-              key={url}
-              alt="person"
-              width={52}
-              height={52}
+    <div className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl transition-all duration-1000 ease-in-out`}>
+      <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10">
+        <div className="flexCenter gap-4">
+          <div className="rounded-full bg-green-50 p-4">
+            <Image
+              src="/folded-map.svg"
+              alt="map"
+              width={28}
+              height={28}
             />
-          ))}
-        </span>
-        <p className="bold-16 md:bold-20 text-white">{peopleJoined}</p>
+          </div>
+          <div className="flex flex-col gap-1">
+            <h4 className="bold-18 text-white">{title}</h4>
+            <p className="regular-14 text-white">{subtitle}</p>
+          </div>
+        </div>
+
+        <div className="flexCenter gap-6">
+          <span className="flex -space-x-4 overflow-hidden">
+            {PEOPLE_URL.map((url) => (
+              <Image 
+                className="inline-block h-10 w-10 rounded-full"
+                src={url}
+                key={url}
+                alt="person"
+                width={52}
+                height={52}
+              />
+            ))}
+          </span>
+          <p className="bold-16 md:bold-20 text-white">{peopleJoined}</p>
+        </div>
       </div>
-     </div>
     </div>
-  )
-}
+  );
+};
 
 const Camp = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -64,12 +64,6 @@ const Camp = () => {
       subtitle: "Somewhere in the Wilderness",
       peopleJoined: "50+ Joined",
     },
-    {
-      backgroundImage: "bg-bg-img-3",
-      title: "River Side Camp",
-      subtitle: "Lush Green Valley",
-      peopleJoined: "50+ Joined",
-    },
   ];
 
   useEffect(() => {
@@ -82,34 +76,28 @@ const Camp = () => {
 
   return (
     <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
-      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
-        <CampSite 
-          backgroundImage="bg-bg-img-1"
-          title="Putuk Truno Camp"
-          subtitle="Prigen, Pasuruan"
-          peopleJoined="50+ Joined"
-        />
-        <CampSite 
-          backgroundImage="bg-bg-img-2"
-          title="Mountain View Camp"
-          subtitle="Somewhere in the Wilderness"
-          peopleJoined="50+ Joined"
-        />
-        <CampSite 
-          backgroundImage="bg-bg-img-3"
-          title="Mountain View Camp"
-          subtitle="Somewhere in the Wilderness"
-          peopleJoined="50+ Joined"
-        />
-      </div>
+        <div 
+          className="flex transition-transform duration-1000 ease-in-out  h-[340px] w-auto items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]"
+          style={{ transform: `translateX(-${currentIndex * 20}%)` }}
+        >
+          {campsites.map((campsite, index) => (
+            <CampSite
+              key={index}
+              backgroundImage={campsite.backgroundImage}
+              title={campsite.title}
+              subtitle={campsite.subtitle}
+              peopleJoined={campsite.peopleJoined}
+            />
+          ))}
+        </div>
 
       <div className="flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
         <div className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
           <h2 className="regular-24 md:regular-32 2xl:regular-64 capitalize text-white">
-            <strong>Feeling Lost</strong> And Not Knowing The Way?
+          <strong>Exceptional Service</strong> & <strong>Unforgettable Events</strong>
           </h2>
           <p className="regular-14 xl:regular-16 mt-5 text-white">
-            Starting from the anxiety of the climbers when visiting a new climbing location, the possibility of getting lost is very large. That's why we are here for those of you who want to start an adventure
+          Whether you're enjoying our warm and attentive service in our restaurant or celebrating a special occasion in our beautiful outdoor setting, we're dedicated to making every experience memorable. From the joy of our waitstaff serving delicious meals with a smile to hosting your events amidst nature's beauty, we ensure every detail is perfect for you.
           </p>
           <Image 
             src="/quote.svg"
@@ -121,7 +109,7 @@ const Camp = () => {
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Camp
+export default Camp;
